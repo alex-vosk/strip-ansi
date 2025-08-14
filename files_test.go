1d78b84731e516b7197d6105961efa7fe9287444
Alex Voskoboynik <145721629+alex-vosk@users.noreply.github.com>
2025-08-14 16:25:02 -0700

    test: simplified files unit tests (#16)

--- files_test.go
+++ files_test.go
@@ -6,53 +6,50 @@ import (
 	"testing"
 )
 
-func TestMakeInputOutputFiles(t *testing.T) {
-	t.Run("default file", func(t *testing.T) {
-		f, err := makeFile(stdInOut, os.Stderr, nil)
-		if err != nil {
-			t.Fatalf("unexpected error: %v", err)
-		}
-		if f != os.Stderr {
-			t.Error("expected file for input")
-		}
+func TestMakeFileDefault(t *testing.T) {
+	f, err := makeFile(stdInOut, os.Stderr, nil)
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	if f != os.Stderr {
+		t.Error("expected file for input")
+	}
+}
+
+func TestMakeFileSuccess(t *testing.T) {
+	const fileName = "/some/file/name"
+	actualFileName := ""
+	f, err := makeFile(fileName, os.Stderr, func(fn string) (*os.File, error) {
+		actualFileName = fn
+		return os.Stdin, nil
 	})
 
-	t.Run("make file - success", func(t *testing.T) {
-		const fileName = "/some/file/name"
-		actualFileName := ""
-		f, err := makeFile(fileName, os.Stderr, func(fn string) (*os.File, error) {
-			actualFileName = fn
-			return os.Stdin, nil
-		})
-
-		if err != nil {
-			t.Fatalf("unexpected error: %v", err)
-		}
-		if f != os.Stdin {
-			t.Error("expected file for input")
-		}
-		if actualFileName != fileName {
-			t.Error("unexpected filename for input")
-		}
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	if f != os.Stdin {
+		t.Error("expected file for input")
+	}
+	if actualFileName != fileName {
+		t.Error("unexpected filename for input")
+	}
+}
+func TestMakeFileFailure(t *testing.T) {
+	const fileName = "/some/file/name"
+	const errMsg = "Can't open file"
+	actualFileName := ""
+	f, err := makeFile(fileName, os.Stderr, func(fn string) (*os.File, error) {
+		actualFileName = fn
+		return nil, errors.New(errMsg)
 	})
 
-	t.Run("make file - error", func(t *testing.T) {
-		const fileName = "/some/file/name"
-		const errMsg = "Can't open file"
-		actualFileName := ""
-		f, err := makeFile(fileName, os.Stderr, func(fn string) (*os.File, error) {
-			actualFileName = fn
-			return nil, errors.New(errMsg)
-		})
-
-		if err == nil || err.Error() != errMsg {
-			t.Fatalf("unexpected error: %v", err)
-		}
-		if f != nil {
-			t.Error("expected file for input")
-		}
-		if actualFileName != fileName {
-			t.Error("unexpected filename for input")
-		}
-	})
+	if err == nil || err.Error() != errMsg {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	if f != nil {
+		t.Error("expected file for input")
+	}
+	if actualFileName != fileName {
+		t.Error("unexpected filename for input")
+	}
 }
